d846d3ddcc8641e0920991ff992a6e5229657288
wilfreidlando <wilfreidlando@gmail.com>
2024-12-10 12:22:52 +0100

    Initial version of AlertBug package

--- README.md
+++
@@ -1,2 +0,0 @@
-# exception-reporter
-ce projet est un projet est un package laravel conçu pour capturer les erreurs et envoyer une une application externe via une api
--- src/config/alertbug.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'api_key' => env('ALERTBUG_API_KEY', ''),
-    'api_url' => env('ALERTBUG_API_URL', ''),
-    'enabled' => env('ALERTBUG_ENABLED', true),
-];
